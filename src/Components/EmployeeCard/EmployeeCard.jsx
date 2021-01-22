3e7d8b4ac7b18ce8e674503a76d231f7cd1a83dd
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2021-01-21 22:17:25 -0500

    extracted the employee card to it's own component and added props

--- src/Components/EmployeeCard/EmployeeCard.jsx
+++ src/Components/EmployeeCard/EmployeeCard.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import "./style.css";
 
-const EmployeeCard = () => {
+const EmployeeCard = (props) => {
     return (
-        <div>
-            
-        </div>
+        <div className="card" style={{ width: "18rem" }}>
+            <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+            <h5 className="card-title">{props.first}</h5>
+                <p className="card-text">
+                    {props.age}
+                </p>
+            </div>
+         </div>
     );
 };
 
--- src/Container/Home/Home.jsx
+++ src/Container/Home/Home.jsx
@@ -1,26 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import EmployeeCard from "../../Components/EmployeeCard/EmployeeCard"
 
 const Home = () => {
 
-    const [info, setInfo] = useState([
-        {
-            gender: "female",
-            title: "Mrs",
-            first: "Lotta",
-            last: "Aalto",
-            email: "lotta.aalto@example.com",
-            age: "35",
-        },
-    ]);
+    const [info, setInfo] = useState([]);
 
-    // useEffect(() => {
+    useEffect(() => {
 
-    //     axios.get("https://randomuser.me/api/?results=10").then((response) => {
-    //         console.log(response.data.data);
-    //         setInfo(response.data.data);
-    //     });
-    // }, {});
+        axios.get("https://randomuser.me/api/?results=10").then((response) => {
+            console.log(response.data.results);
+            setInfo(response.data.results);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, []);
 
     return (
         <div className="container">
@@ -30,17 +25,8 @@ const Home = () => {
                 </div>
             </div>
             <div className="row">
-             {/* This is where the employees go */}
              {info.map((info) => (
-                  <div className="card" style={{ width: "18rem" }}>
-                  <img src="..." className="card-img-top" alt="..."/>
-                  <div className="card-body">
-                   <h5 className="card-title">{info.first, info.last}</h5>
-                      <p className="card-text">
-                          {info.age}
-                      </p>
-                    </div>
-              </div>
+                 <EmployeeCard key={info.results.id.value} name={info.results.name.first} last={info.results.name.last} age={info.results.dob.age}/> 
              ))}
              </div>
         </div>
